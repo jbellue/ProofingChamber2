7dd4041ac22b0bc7a21b8b328eb0e3aa8be8c8e2
Julien Bellue <julien.bellue@gmail.com>
2025-03-28 23:22:45 +0100

    Update encoder switch pin definition for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 
 #define ENCODER_CLK 2
 #define ENCODER_DT  3
-#define ENCODER_SW  4
+#define ENCODER_SW  10
 
 // Global objects
 U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
