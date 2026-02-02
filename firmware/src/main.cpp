695b4f47eef706750b0a5c5d747cdaf91da8a050
jbellue <julien@bellue.fr>
2026-02-02 12:37:10 +0100

    Update PCB

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -35,14 +35,14 @@
 #include "TemperatureController.h"
 #include "AppContextDecl.h"
 
-#define DS18B20_PIN       GPIO_NUM_0
-#define COOLING_RELAY_PIN GPIO_NUM_20 //update prototype
-#define HEATING_RELAY_PIN GPIO_NUM_6  //update prototype
-#define ENCODER_CLK       GPIO_NUM_3
-#define ENCODER_DT        GPIO_NUM_4
+#define DS18B20_PIN       GPIO_NUM_20 //update prototype
+#define COOLING_RELAY_PIN GPIO_NUM_2  //update prototype
+#define HEATING_RELAY_PIN GPIO_NUM_1  //update prototype
+#define ENCODER_CLK       GPIO_NUM_6  //update prototype
+#define ENCODER_DT        GPIO_NUM_7  //update prototype
 #define ENCODER_SW        GPIO_NUM_5
-#define PROOFING_LED_PIN  GPIO_NUM_1 //update prototype
-#define COOLING_LED_PIN   GPIO_NUM_10
+#define PROOFING_LED_PIN  GPIO_NUM_10 //update prototype
+#define COOLING_LED_PIN   GPIO_NUM_21 //update prototype
 
 // Global objects
 DisplayManager displayManager(U8G2_R0);
